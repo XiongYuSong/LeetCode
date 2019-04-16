e539081b34bc38d1892a6b6654cee6a2126c1b3f
XiongYuSong <937724308@qq,com>
2019-04-16 11:49:33 +0800

    ListNode

---
+++ src/hue/edu/xiong/ListNode.java
@@ -0,0 +1,14 @@
+package hue.edu.xiong;
+
+/**
+ * @Author Xiong YuSong
+ * @Date 2019/4/16
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
