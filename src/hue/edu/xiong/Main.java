54a9dd1d6755a1b4cb3666eb8c2b7378e4809ac3
XiongYuSong <937724308@qq.com>
2019-05-20 16:51:32 +0800

    123

--- src/hue/edu/xiong/Main.java
+++ src/hue/edu/xiong/Main.java
@@ -14,13 +14,12 @@ public class Main {
         boolean b = m.find(); //返回是否匹配的结果,这是部分匹配
         System.out.println(b);
 
-
         System.out.println(str1.matches("[" + str2 + "^]+") && str2.matches("[" + str1 + "^]+"));
 
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.remove(0);
-
-
+        //居然改不了ubantu下面搜狗输入法的显示框的位置,emmmm,这不是坑爹吗
+        Integer a = Integer.valueOf(1);//这里有问题啊啊啊,这个输入法好奇怪啊,zenmezaiyouxiajiao啊啊啊啊aaaa
     }
 }
