a74de54dd6104e3ef610de3b9455e94563b63afe
XiongYuSong <937724308@qq.com>
2019-07-12 10:20:09 +0800

    main

--- src/hue/edu/xiong/Main.java
+++ src/hue/edu/xiong/Main.java
@@ -1,25 +1,59 @@
 package hue.edu.xiong;
 
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
-    public static void main(String[] args) {
-        String str1 = "listen";
-        String str2 = "silent";
 
-        Pattern p = Pattern.compile("[" + str1 + "]+"); // 正则表达式
-        Matcher m = p.matcher(str2); // 操作的字符串
-        boolean b = m.find(); //返回是否匹配的结果,这是部分匹配
-        System.out.println(b);
 
-        System.out.println(str1.matches("[" + str2 + "^]+") && str2.matches("[" + str1 + "^]+"));
+//    public static void main(String[] args) {
+//        // 每100毫秒创建100线程，每个线程创建一个1M的对象，即每100ms申请100M堆空间
+//        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> {
+//            for (int i = 0; i < 100; i++) {
+//                new Thread(() -> {
+//                    try {
+//                        //  申请1M
+//                        byte[] temp = new byte[1024 * 1024];
+//                        Thread.sleep(new Random().nextInt(1000)); // 随机睡眠1秒以内
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }).start();
+//            }
+//        }, 1000, 100, TimeUnit.MILLISECONDS);
+//    }
 
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.remove(0);
-        //居然改不了ubantu下面搜狗输入法的显示框的位置,emmmm,这不是坑爹吗
-        Integer a = Integer.valueOf(1);//这里有问题啊啊啊,这个输入法好奇怪啊,zenmezaiyouxiajiao啊啊啊啊aaaa
+    public static void main(String[] args) throws ClassNotFoundException {
+        ReentrantLock reentrantLock = new ReentrantLock();
+        AtomicInteger atomicInteger = new AtomicInteger();
+        LongAdder longAdder = new LongAdder();
+        //没有4,和有很多重复的7
+        int[] arr1 = new int[]{1, 2, 3, 5, 6, 7, 7, 7, 7, 8, 9, 10, 11, 13, 14};
+        int target = 7;
+        int index = fun1(arr1, target, 0, arr1.length - 1);
+        System.out.println("下标为: " + index + " ,值为: " + arr1[index]);
+        index = fun2(arr1, target, 0, arr1.length - 1);
+        System.out.println("下标为: " + index + " ,值为: " + arr1[index]);
+    }
+
+    //二分查找法模板一:查找等于或者小于target的最后一个值
+    private static int fun2(int[] arr, int target, int l, int r) {
+        while (l < r) {
+            int mid = (l + r + 1) >> 1;
+            if (target < arr[mid]) r = mid - 1;
+            else l = mid;
+        }
+        return l;
+    }
+
+    //二分查找法模板二:查找等于或者大于target的第一个值
+    private static int fun1(int[] arr, int target, int l, int r) {
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (target > arr[mid]) l = mid + 1;
+            else r = mid;
+        }
+        return l;
     }
 }
